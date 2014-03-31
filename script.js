9cf6ee5be21c611db043bb3a033454483395ea4a
Exequiel <exefire@gmail.com>
2014-03-31 01:22:26 -0300

    No se borra

--- script.js
+++ script.js
@@ -1,71 +1,78 @@
-// JavaScript Document
 // Wait for device API libraries to load
-//
-document.addEventListener("deviceready", onDeviceReady, false);
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
 
-// Populate the database
-//
-function populateDB(tx) {
-		var currentdate = new Date(); 
-		var hora = 				"" 
+    // Populate the database
+    //
+    function populateDB(tx) {
+			var currentdate = new Date(); 
+			var hora = 				"" 
 										+ currentdate.getFullYear() + "-"  
 										+ (currentdate.getMonth()+1)  + "-" 
 										+	currentdate.getDate() + " "
 										+ currentdate.getHours() + ":"  
 										+ currentdate.getMinutes() + ":" 
 										+ currentdate.getSeconds();
-		tx.executeSql('DROP TABLE IF EXISTS DEMO');
-		tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
-		tx.executeSql('INSERT INTO DEMO (id, data) VALUES (1, "'+hora+'")');
-		tx.executeSql('INSERT INTO DEMO (id, data) VALUES (2, "Second row")');
-		tx.executeSql('INSERT INTO DEMO (id, data) VALUES (3, "Tercera Compañía")');
-		tx.executeSql('INSERT INTO DEMO (id, data) VALUES (4, "4º Compañía")');
-		tx.executeSql('INSERT INTO DEMO (id, data) VALUES (5, "5ª Compañía")');
-}
-
-// Query the database
-//
-function queryDB(tx) {
-		tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
-}
-
-// Query the success callback
-//
-function querySuccess(tx, results) {
-    var len = results.rows.length;
-		console.log("Returned rows = " + results.rows.length);
-		// this will be true since it was a select statement and so rowsAffected was 0
+        //tx.executeSql('DROP TABLE IF EXISTS DEMO');
+				tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id unique, data)');
+				queryDB2(tx);
+			}
 		
-		$('#contenido').html('');
-		for (var i=0; i<len; i++){
-				console.log("Row = " + i + " ID = " + results.rows.item(i).id + " Data =  " + results.rows.item(i).data);
-				$('#contenido').append("Row = " + i + " ID = " + results.rows.item(i).id + " Data =  " + results.rows.item(i).data + '<br>');
+	 function queryDB2(tx) {
+        return tx.executeSql('SELECT max(id) as id FROM DEMO ORDER BY id DESC LIMIT 1',[],querySuccess2);
+    }
+		
+		function querySuccess2(tx, results) {
+			var currentdate = new Date(); 
+			var hora = 				"" 
+										+ currentdate.getFullYear() + "-"  
+										+ (currentdate.getMonth()+1)  + "-" 
+										+	currentdate.getDate() + " "
+										+ currentdate.getHours() + ":"  
+										+ currentdate.getMinutes() + ":" 
+										+ currentdate.getSeconds();
+			var siguiente = results.rows.item(0).id + 1;
+			tx.executeSql('INSERT INTO DEMO (id, data) VALUES ('+siguiente+', "'+hora+'")');
 		}
 		
-		if (!results.rowsAffected) {
-				console.log('No rows affected!');
-				return false;
-		}
-		// for an insert statement, this property will return the ID of the last inserted row
-		console.log("Last inserted row ID = " + results.insertId);
-}
+    // Query the database
+    //
+    function queryDB(tx) {
+        tx.executeSql('SELECT * FROM DEMO', [], querySuccess, errorCB);
+    }
 
-// Transaction error callback
-//
-function errorCB(err) {
-		console.log("Error processing SQL: "+err.code);
-}
+    // Query the success callback
+    //
+    function querySuccess(tx, results) {
+        var len = results.rows.length;
+        console.log("DEMO table: " + len + " rows found.");
+				$('#contenido').html('');
+        for (var i=0; i<len; i++){
+						var txt = "Row = " + i + " ID = " + results.rows.item(i).id + " Data =  " + results.rows.item(i).data;
+            console.log(txt);
+						$('#contenido').append(txt);
+						$('#contenido').append('<br>');
+        }
+    }
 
-// Transaction success callback
-//
-function successCB() {
-		var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-		db.transaction(queryDB, errorCB);
-}
+    // Transaction error callback
+    //
+    function errorCB(err) {
+        console.log("Error processing SQL: "+err.message);
+    }
 
-// device APIs are available
-//
-function onDeviceReady() {
-		var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
-		db.transaction(populateDB, errorCB, successCB);
-}
+    // Transaction success callback
+    //
+    function successCB() {
+        var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+        db.transaction(queryDB, errorCB);
+    }
+
+    // device APIs are available
+    //
+    function onDeviceReady() {
+        var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+        db.transaction(populateDB, errorCB, successCB);
+    }
+        var db = window.openDatabase("Database", "1.0", "Cordova Demo", 200000);
+        db.transaction(populateDB, errorCB, successCB);
